fa123688b66c11fa351ae2e4bf0d18ceb6d214ab
Felipe <angelozifelipe@gmail.com>
2023-08-29 21:08:18 -0300

    Finalizando curso de boas práticas e segurança na API Rest

---
+++ src/main/java/med/voll/api/tools/security/SecurityFilter.java
@@ -0,0 +1,51 @@
+package med.voll.api.tools.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.user.model.User;
+import med.voll.api.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public SecurityFilter(TokenService tokenService,
+                          UserRepository userRepository) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenJWT = tokenRecovery(request);
+        if (Objects.nonNull(tokenJWT)) {
+            String subject = tokenService.getSubject(tokenJWT);
+            UserDetails user = userRepository.findByLogin(subject);
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        };
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String tokenRecovery(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if(Objects.isNull(authorizationHeader)) return authorizationHeader.replace("Bearer", "");
+        return null;
+    }
+}
